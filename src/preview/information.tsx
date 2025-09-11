5e495069156a7e84488b85b9ceaf4a13b577c528
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2025-09-11 15:58:30 +0200

    info comp

---
+++ src/preview/information.tsx
@@ -0,0 +1,8 @@
+export function Information() {
+  return (
+    <div className="pt-40 pl-10">
+      <a href="#">Component Link</a>
+      <a href="#non-existing">implementation</a>
+    </div>
+  );
+}
